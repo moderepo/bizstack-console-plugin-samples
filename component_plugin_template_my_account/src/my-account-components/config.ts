fe0e41820ae22a77327653be202346df0fe913f5
bill-mode <53446239+bill-mode@users.noreply.github.com>
2024-07-24 09:44:08 -0700

    install the latest bizconsole sdk (#6)

--- component_plugin_template_my_account/src/my-account-components/config.ts
+++ component_plugin_template_my_account/src/my-account-components/config.ts
@@ -9,7 +9,7 @@ import { LogoutButton, isLogoutButtonProps } from './LogoutButton';
  * load the plugin, it will be looking for this object.
  */
 export const bizConsolePlugins: BizConsolePluginPack = {
-    bizConsoleVersion: '0.0.4',
+    bizConsoleVersion: '1.0.0-alpha.0',
     plugins: [
         {
             type: BizConsolePluginType.COMPONENT,
