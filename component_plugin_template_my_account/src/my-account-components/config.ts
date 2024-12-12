a2bb4b4d1f8959c3166f6e391d5c6c584c30360f
Jun Takeda <7824306+takjn@users.noreply.github.com>
2024-12-12 17:17:23 +0900

    bump SDK from 1.4.1 to 1.5.0

--- component_plugin_template_my_account/src/my-account-components/config.ts
+++ component_plugin_template_my_account/src/my-account-components/config.ts
@@ -10,7 +10,7 @@ import { CustomMapComponent, isCustomMapComponentProps } from './CustomMapCompon
  * load the plugin, it will be looking for this object.
  */
 export const bizConsolePlugins: BizConsolePluginPack = {
-    bizConsoleVersion: '1.4.1',
+    bizConsoleVersion: '1.5.0',
     namespace: 'SampleMyAccountCompsPlugin',
     plugins: [
         {
