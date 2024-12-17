94a91c4822ea962cbec215214b7ea76b11b8d6d6
Jun Takeda <7824306+takjn@users.noreply.github.com>
2024-12-17 09:21:11 +0900

    bump SDK from 1.4.1 to 1.5.0 (#37)

--- widget_plugin_template_weather_widget/src/weather-widgets/config.ts
+++ widget_plugin_template_weather_widget/src/weather-widgets/config.ts
@@ -26,7 +26,7 @@ import {
  * load the plugin, it will be looking for this object.
  */
 export const bizConsolePlugins: BizConsolePluginPack = {
-    bizConsoleVersion: '1.4.1',
+    bizConsoleVersion: '1.5.0',
     namespace: 'SampleWeatherWidgetPlugin',
     plugins: [
         {
