e0d5b1e7e3fdb5cd692b2ca55272f5308ad071cf
Takayama Fumihiko <tekezo@pqrs.org>
2025-10-22 14:28:46 +0900

    Add src/dev

---
+++ widget_plugin_template_weather_widget/src/dev/providers/DateLocalizationProvider.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { jaJP } from '@mui/x-date-pickers/locales';
+import ja from 'date-fns/locale/ja';
+import { PropsWithChildren } from 'react';
+
+/**
+ * Localization Provider
+ */
+export const DateLocalizationProvider = ({ children }: PropsWithChildren) => {
+  return (
+    <LocalizationProvider
+      dateAdapter={AdapterDateFns}
+      adapterLocale={ja}
+      localeText={jaJP.components.MuiLocalizationProvider.defaultProps.localeText}
+      dateFormats={{ year: 'yyyy年' }}
+    >
+      {children}
+    </LocalizationProvider>
+  );
+};
---
+++ widget_plugin_template_weather_widget/src/dev/routes/BizConsoleRoutes.tsx
@@ -0,0 +1,166 @@
+import { Suspense } from 'react';
+import { Route, RouteObject, Routes, useNavigate, useParams } from 'react-router-dom';
+import { Box, Drawer, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import {
+    DEFAULT_ROUTE_PATHS,
+    LoadingScreen,
+    useAuthenticationStore,
+    useGetParamValueFromRouteParamsAsNumber,
+    IEntityViewComponentsManager,
+} from '@moderepo/bizstack-console-sdk';
+import { TemperatureWidget } from '../../weather-widgets/TemperatureWidget';
+import { HumidityWidget } from '../../weather-widgets/HumidityWidget';
+import { createInitialTemperatureWidgetSettings } from '../../weather-widgets/models';
+
+const PageNotFoundPage: React.FC = () => {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                minHeight: '100vh',
+                flexDirection: 'column',
+                fontSize: 40,
+            }}
+        >
+            <Box>404 Page Not Found</Box>
+            <Box>Please open {'http://localhost:5001/projects/{projectId}/custom/my_custom_page'}</Box>
+        </Box>
+    );
+};
+
+const drawerWidth = 200;
+
+const CustomDrawer = () => {
+    const projectId = useGetParamValueFromRouteParamsAsNumber('projectId');
+    const navigate = useNavigate();
+
+    return (
+        <Drawer
+            variant="permanent"
+            open={true}
+            sx={{
+                flexShrink: 0,
+                width: drawerWidth,
+            }}
+        >
+            <List>
+                <ListItem>
+                    <ListItemButton onClick={() => navigate(`/projects/${projectId}/custom/my_custom_page`)}>
+                        <ListItemText>my_custom_page</ListItemText>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem>
+                    <ListItemButton onClick={() => navigate(`/projects/${projectId}/custom/my_custom_page_2/100000?startTime=946684800000`)}>
+                        <ListItemText>my_custom_page2</ListItemText>
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Drawer>
+    );
+};
+
+const mockComponentsManager: IEntityViewComponentsManager = {
+    hasLoadedDataForAllComponents: () => false,
+    removeComponent: (compId: string) => {
+        void compId;
+    },
+    loadDataForComponent: (compId: string, timeParams, selectionRange) => {
+        void compId;
+        void timeParams;
+        void selectionRange;
+    },
+    createComponent: (params) => {
+        void params;
+        return <></>;
+    },
+    createComponentForEditMode: (params) => {
+        void params;
+        return <></>;
+    },
+};
+
+const MyCustomPage1Route: React.FC = () => {
+    const params = useParams();
+    const loggedInUserId = useAuthenticationStore((state) => state.authInfo?.userId ?? state.loggedInUser?.id ?? 0);
+
+    return (
+        <TemperatureWidget
+            compSettings={createInitialTemperatureWidgetSettings('TemperatureWidget')}
+            componentsManager={mockComponentsManager}
+            loggedInUserId={loggedInUserId}
+            id="temperature-widget-1"
+            projectId={params.projectId !== undefined ? Number(params.projectId) : Number.NaN}
+            entityId="test"
+            componentSettingsRef={{ componentType: 'SampleWeatherWidgetPlugin.TemperatureWidget' }}
+            componentParentSettingsRef={undefined}
+        />
+    );
+};
+
+const MyCustomPage2Route: React.FC = () => {
+    const params = useParams();
+    const loggedInUserId = useAuthenticationStore((state) => state.authInfo?.userId ?? state.loggedInUser?.id ?? 0);
+
+    return (
+        <HumidityWidget
+            compSettings={createInitialTemperatureWidgetSettings('HumidityWidget')}
+            componentsManager={mockComponentsManager}
+            loggedInUserId={loggedInUserId}
+            id="humidity-widget-1"
+            projectId={params.projectId !== undefined ? Number(params.projectId) : Number.NaN}
+            entityId="test"
+            componentSettingsRef={{ componentType: 'SampleWeatherWidgetPlugin.HumidityWidget' }}
+            componentParentSettingsRef={undefined}
+        />
+    );
+};
+
+export const bizConsoleRoutes: RouteObject[] = [
+    {
+        path: DEFAULT_ROUTE_PATHS.PROJECT_$projectId_CUSTOM,
+        element: (
+            <Box sx={{ display: 'flex', height: '100%' }}>
+                <CustomDrawer />
+
+                <Box>
+                    <Routes>
+                        <Route
+                            path={`my_custom_page`}
+                            element={
+                                <Suspense fallback={<LoadingScreen open={true} />}>
+                                    <MyCustomPage1Route />
+                                </Suspense>
+                            }
+                        />
+                        <Route
+                            path={`my_custom_page_2/:entityId`}
+                            element={
+                                <Suspense fallback={<LoadingScreen open={true} />}>
+                                    <MyCustomPage2Route />
+                                </Suspense>
+                            }
+                        />
+                        <Route
+                            path="*"
+                            element={
+                                <Suspense fallback={<LoadingScreen open={true} />}>
+                                    <PageNotFoundPage />
+                                </Suspense>
+                            }
+                        />
+                    </Routes>
+                </Box>
+            </Box>
+        ),
+    },
+    {
+        path: '*',
+        element: (
+            <Suspense fallback={<LoadingScreen open={true} />}>
+                <PageNotFoundPage />
+            </Suspense>
+        ),
+    },
+];
